7edb8275846e7ac7a5fb7202ba7505092b4e8fde
Tanguille <Tanguille.grootaert@student.hogent.be>
2021-10-30 19:10:31 +0200

    Eindelijk dark mode

--- Oefeningen D05/Oefeningen D05/Oefening D05.gemiddelde.cs
+++ Oefeningen D05/Oefeningen D05/Oefening D05.gemiddelde.cs
@@ -7,19 +7,24 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int som = 0;
+            int aantal = 0;
             int getal = 0;
-                         
+                                     
             do
             {
                 Console.Write("Geef een getal: ");
                 string getalAlsTekst = Console.ReadLine();
                 getal = int.Parse(getalAlsTekst);
                 som += getal;
+                aantal++;
+
             } while (getal != -1);
 
             som -= getal;
-
-        Console.Write($"De som is {som}.");
+            aantal--;
+            
+            double gemiddelde = som / aantal;
+            Console.Write($"Het gemiddelde is {gemiddelde}.");
 
         }
     }
